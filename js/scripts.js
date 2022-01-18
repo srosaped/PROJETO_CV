3310230c97197c69b45907a463d1e9d55c7517e9
Sergio Pedro <srosaped@outlook.com>
2022-01-18 17:19:23 +0000

    adds navigation trought sections

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,31 @@
-/* Hamburguer Menu Animation */
 $(document).ready(function () {
 
+/* Hamburguer Menu Animation */
     $('.hamburguer').on('click', function () {
   
       $('.animated-icon').toggleClass('open');
     });
-    
+
+/* Smooth Scroll  */
+    $("a").on('click', function(event) {
+
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      } 
+    });
+
   });
+
+  const scrollto = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
+    
