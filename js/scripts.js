adbf3d46e5c0d7c44b409b304e3c76439990be65
Sergio Pedro <srosaped@outlook.com>
2022-01-19 15:18:40 +0000

    Adds skills finished animation

--- js/scripts.js
+++ js/scripts.js
@@ -22,19 +22,44 @@ $(document).ready(function () {
 
      /* Skills Animation */
     $("#skills").mouseover(function() {
-    $(".prog1").animate({
-      width: "70%"
-    }, 2500);
+        $(".prog1").animate({
+          width: "75%"
+        }, 250);
+        $(".prog2").animate({
+          width: "70%"
+        }, 250);
+        $(".prog3").animate({
+          width: "50%"
+        }, 250);
+        $(".prog4").animate({
+          width: "70%"
+        }, 250);
+        $(".prog5").animate({
+          width: "40%"
+        }, 250);
+        $(".prog6").animate({
+          width: "45%"
+        }, 250);
+        $(".prog7").animate({
+          width: "30%"
+        }, 250);
+        $(".prog8").animate({
+          width: "20%"
+        }, 250);
     });
-
+    
     $("#skills").mouseout(function() {
-    $(".prog1").stop();
+      $(".prog1",).stop();/* Stop Animation Loop */
+      $(".prog2").stop();
+      $(".prog3").stop();
+      $(".prog4").stop();
+      $(".prog5").stop();
+      $(".prog6").stop();
+      $(".prog7").stop();
+      $(".prog8").stop();
     });
 
   });
-
-
- 
 
   /* Smoth Scroll to all browsers */
   const scrollto = () => {
